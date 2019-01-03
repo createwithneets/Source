fbd1a3b57a3ad2bde69dfe4cc469cf241c5f6732
Anita Cheung <hello@anitacheung.ca>
2019-01-02 18:56:26 -0800

    figured out how to get carrierwave working and added adminpanel for images and added items index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+#items controller 
+resources :items
+
 
 resources :users
 
