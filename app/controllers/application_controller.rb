388357550630e9a59a191a6b3681393f84dd0bd4
Anita Cheung <hello@anitacheung.ca>
2019-01-02 19:12:53 -0800

    force login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,12 @@ def is_logged_in?
   session[:user_id].present?
 end
 
+def force_login
+  unless is_logged_in?
+    flash[:error] = "You are not logged in"
+    redirect_to new_user_path
+  end
+end
+
 
 end
