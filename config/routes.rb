eaa4a928e42e7b02e77515241920df13e686d54d
Anita Cheung <hello@anitacheung.ca>
2019-01-03 18:43:35 -0800

    added categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-#items controller 
+
+
+#categoires controller
+
+resources :categories
+#items controller
 resources :items
 
 
