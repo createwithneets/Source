e7af53c23fa22f206a20a7d08ee706570d4edc10
Anita Cheung <hello@anitacheung.ca>
2019-01-04 16:49:41 -0800

    experimented with tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+#class UserTest < ActiveSupport::TestCase
+   #test "should not save when user is blank" do
+    # assert_equal User.new.save, false
+
+   #end
+
+
+   #test "should save if user is full" do
+   #@user = users(:neets)
+   #matches up with fixture file
+  # assert_equal @user.save true
+   #end
+
+   #test "should save and subscribe" do
+  #   @user = users(:neets)
   # end
-end
+  # end
